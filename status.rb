f1468d5e5f2d236c497e232d14c3edaa0aa67ad2
Mike Harris <mharris717@gmail.com>
2019-02-27 12:56:59 -0500

    CS

--- status.rb
+++ status.rb
@@ -10,6 +10,8 @@ end
 ec "pwd && ls -al && env"
 
 if ENV['GITHUB_REF'] == 'refs/heads/master'
+  ec 'git config --global user.email "docs@liveramp.com"'
+  ec 'git config --global user.name "Docs"'
   ec 'date > current.txt'
   ec 'git add current.txt'
   ec 'git commit -m "current date"'
