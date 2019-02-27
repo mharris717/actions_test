c704b40f97497e2d09986634f090d361b75c4548
Mike Harris <mharris717@gmail.com>
2019-02-27 13:00:06 -0500

    CS

--- status.rb
+++ status.rb
@@ -15,7 +15,7 @@ if ENV['GITHUB_REF'] == 'refs/heads/master'
   ec 'date > current.txt'
   ec 'git add current.txt'
   ec 'git commit -m "current date"'
-  ec 'git push origin master:docs'
+  ec 'git push origin master:docs -f'
 else
   puts "not master"
 end
